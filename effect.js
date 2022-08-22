ca812fba81de819d8af203bd7c7115c29db427a6
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2022-08-22 15:27:59 -0400

    to read me

--- effect.js
+++ effect.js
@@ -5,7 +5,7 @@ saveBtn = document.querySelector(".save-btn");
 
 selectMenu.addEventListener("change", () => {
     const selectedFormat = selectMenu.options[selectMenu.selectedIndex].text;
-    saveBtn.innerText = `Save As ${selectedFormat.split(" ")[0]} File`;
+    saveBtn.innerText = `Guardar archivo como ${selectedFormat.split(" ")[0]}`;
 });
 
 saveBtn.addEventListener("click", () => {
